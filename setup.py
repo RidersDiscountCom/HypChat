375365f8a08146dbd61e239bdf164d4a1d54580f
James Bliss <james@ridersdiscount.com>
2013-12-12 17:04:32 -0500

    Cleaning up after paul

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='hypchat',
       classifiers=[ # https://pypi.python.org/pypi?%3Aaction=list_classifiers
             'Development Status :: 2 - Pre-Alpha',
             'Intended Audience :: Developers',
-            'License :: OSI Approved :: BSD License',
+            'License :: OSI Approved :: MIT License',
             'Operating System :: OS Independent',
             'Programming Language :: Python',
             'Programming Language :: Python :: 2 :: Only',
