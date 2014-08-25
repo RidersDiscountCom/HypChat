a8b54558e8ffc6610df32f37f9e4ae0a516d5d18
Grissess <grissess@nexusg.org>
2014-08-24 22:12:29 -0400

    Made keyword arguments pass through as query parameters for all Linker calls

--- hypchat/restobject.py
+++ hypchat/restobject.py
@@ -65,16 +65,17 @@ class Linker(object):
 				return obj
 		return json.JSONDecoder(object_hook=_object_hook).decode(text)
 
-	def __call__(self, expand=None):
+	def __call__(self, expand=None, **kwargs):
 		"""
 		Actually perform the request
 		"""
-		params = None
+		params = {}
 		if expand is not None:
 			if isinstance(expand, basestring):
 				params = {'expand': expand}
 			else:
 				params = {'expand': ','.join(expand)}
+		params.update(kwargs)
 
 		rv = self._obj_from_text(self._requests.get(self.url, params=params).text, self._requests)
 		rv._requests = self._requests
