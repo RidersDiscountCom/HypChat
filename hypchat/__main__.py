3e12f64410726eb0c52d89a58cb6bc308136d4c4
Toshiaki Baba <toshiaki@netmark.jp>
2014-10-09 19:50:24 +0900

    endpoint enable with cli like usage

--- hypchat/__main__.py
+++ hypchat/__main__.py
@@ -16,12 +16,23 @@ else:
           '~/.hypchat, /etc/hypchat, or the environment variable HIPCHAT_TOKEN.')
     sys.exit(1)
 
-hipchat = HypChat(AUTH_TOKEN)
+ENDPOINT = None
+if config.has_section('HipChat'):
+    ENDPOINT = config.get('HipChat', 'endpoint')
+
+elif 'HIPCHAT_ENDPOINT' in os.environ:
+    ENDPOINT = os.environ['HIPCHAT_ENDPOINT']
+
+if ENDPOINT:
+    hipchat = HypChat(AUTH_TOKEN, ENDPOINT)
+else:
+    hipchat = HypChat(AUTH_TOKEN)
 
 capabilities = hipchat.capabilities
 emoticons = hipchat.emoticons
 rooms = hipchat.rooms
 users = hipchat.users
+endpoint = hipchat.endpoint
 
 try:
     import IPython
